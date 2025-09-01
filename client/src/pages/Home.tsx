af2d23dfb6b1faea71865007f950c46daff5a80f
Bae Kyoung Han <46661432-qorud001@users.noreply.replit.com>
2025-09-01 07:38:48 +0000

    loading image

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -17,16 +17,23 @@ export default function Home() {
       <PRNews />
       {/* 구글 지도 임베드 https://www.google.com/mymaps에서 수정 */}
       <div className="bg-gray-50">
-        <h2 className="text-3xl md:text-4xl font-bold text-uc-deep mb-10 text-center">
+        <h2 className="text-3xl md:text-4xl font-bold text-uc-deep text-center">
           찾아오시는 길
         </h2>
-        <div style={{ position: "relative", width: "100%", height: "500px", overflow: "hidden" }}>
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            height: "500px",
+            overflow: "hidden",
+          }}
+        >
           <iframe
             src="https://www.google.com/maps/d/embed?mid=12oKrUpLaWVwHJm1jAL0fPqiE7z5JiF4&ehbc=2E312F"
             width="100%"
             height="500"
             style={{ border: 0 }}
-          ></iframe>
+          />
 
           <div
             style={{
@@ -34,8 +41,8 @@ export default function Home() {
               top: 0,
               left: 0,
               width: "100%",
-              height: "80px",
-              background: "white",
+              height: "80px", // 가릴 높이
+              background: "#f9fafb",
               zIndex: 10,
             }}
           ></div>
